0659b1f062d8a75da14cf6aef714ada68756d4e3
sachin1801 <sachinadlakha7@gmail.com>
2026-02-03 18:56:40 -0500

    auto scroll for agent session

--- apps/desktop/src/components/agent/AgentWindow.tsx
+++ apps/desktop/src/components/agent/AgentWindow.tsx
@@ -185,6 +185,7 @@ export const AgentWindow: FC<AgentWindowProps> = ({
 			<MessageFeed
 				messageGroups={messageGroups}
 				autoScroll={true}
+				isStreaming={isRunning}
 				onToolApproval={handleToolApproval}
 				className="flex-1"
 			/>
