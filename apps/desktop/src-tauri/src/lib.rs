aa9d54d8e141f1150b7ddbf66d41fe06b2ffea1d
sachin1801 <sachinadlakha7@gmail.com>
2026-04-17 23:55:40 -0400

    chore(voice): add tauri-plugin-notification for dispatch toasts

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -106,6 +106,7 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_macos_permissions::init())
         .plugin(tauri_plugin_decorum::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
